5ea7f3e6c4da993224d44de509e0234a62d91802
Hello <Hello@MY-MINI-PC>
2026-02-13 23:46:46 +0800

    Add project files.

---
+++ first-test-hello-world/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace first_test_hello_world
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            var name = NameTextBox.Text?.Trim();
+            if (string.IsNullOrEmpty(name))
+            {
+                GreetingLabel.Content = string.Empty;
+            }
+            else
+            {
+                GreetingLabel.Content = $"Hello, {name} !!";
+            }
+        }
+    }
+}
